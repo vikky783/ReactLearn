97583bbac0774d6d6492ac0cd0885c4f9b68a263
vikky kumar <loveuvikky2@gmail.com>
2020-06-12 01:32:26 +0530

    React with hook final

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
-    person : [
-      {name: "Vikky", age: 10},
-      {name: "Rikky", age: 20},
-      {name: "Nikky", age: 30},
-      {name: "Mikky", age: 40}
-    ]
-  };
-
-  //Merge comment to test git function
-  //Added comment to test next one
-//updated directlyin git hub
-   
-  switchStateHandler = () =>{
-    this.setState({
+const App = props =>  {
+  
+  const [personState, setPersonState] = useState({
+    
       person : [
-        {name: "New Vikky", age: 10},
+        {name: "Vikky", age: 10},
         {name: "Rikky", age: 20},
         {name: "Nikky", age: 30},
-        {name: "Mikky", age: 100}
+        {name: "Mikky", age: 40}
       ]
-    })
-  }
-  render() {
+    
+    });
+    const switchStateHandler = () =>{
+      setPersonState({
+        person : [
+          {name: "New Vikky", age: 10},
+          {name: "Tikky", age: 20},
+          {name: "Nikky", age: 30},
+          {name: "Mikky", age: 100}
+        ]
+      })
+    } 
+
+   {
     return (
       <div className="App">
        <h1>Hello this is my First React App</h1>
        <p>I am loving it</p>
-       <button onClick={this.switchStateHandler}>Update me</button>
-       <Person name={this.state.person[0].name} age ={this.state.person[0].age}/>
-       <Person name={this.state.person[1].name} age ={this.state.person[1].age}/>
-       <Person name={this.state.person[2].name} age ={this.state.person[2].age}/>
-       <Person name={this.state.person[3].name} age ={this.state.person[3].age}/>
+       <button onClick={switchStateHandler}>Update me</button>
+       <Person name={personState.person[0].name} age ={personState.person[0].age}/>
+       <Person name={personState.person[1].name} age ={personState.person[1].age}/>
+       <Person name={personState.person[2].name} age ={personState.person[2].age}/>
+       <Person name={personState.person[3].name} age ={personState.person[3].age}/>
       </div>
     );
   }
 }
 
 export default App;
+
+//Merge comment to test git function
+//Added comment to test next one
+//updated directlyin git hub
+ 
